6a675bad5117e1bcd1abd62ca072284f08e13629
Tam <enquiretam@gmail.com>
2024-09-01 15:39:24 +0100

    DELETE method working

--- app.py
+++ app.py
@@ -32,7 +32,16 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
-@app.route('')
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Think there was a problem deleting the task Tam :/'
 
 if __name__ == "__main__":
     app.run(debug=True)
